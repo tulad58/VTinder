30f3e74a05ba38509182255edbf06ed1096319c0
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-07-26 14:30:47 +0700

    Remove db and session to session.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from db.session import DBHandler
 from vk.bot import VkBot
 from core.config import settings
+from db.crud import user
 
 if __name__ == '__main__':
-    db = DBHandler()
-    session = db.connect()
     vk_bot = VkBot(access_token=settings.VK_BOT_TOKEN)
     vk_bot.start()
 
