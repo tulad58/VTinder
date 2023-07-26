e2e6a732334ac07fe4da88cabb7a6506d320a361
Nik Bezmaternykh <nikitabit@yandex.ru>
2023-07-26 14:38:52 +0700

    Refactor models

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -12,14 +12,27 @@ class UserFavouriteProfile(Base):
     profile_id = Column(Integer, ForeignKey('favorite_profile.id'), primary_key=True)
 
 
+class UserBlackList(Base):
+    __tablename__ = 'user_black_list'
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    profile_id = Column(Integer, ForeignKey('black_list.id'), primary_key=True)
+
+
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
     user_vk_id = Column(Integer, nullable=False)
+
     favorite_profiles = relationship(
         'FavoriteProfile',
-        secondary=UserFavouriteProfile,
+        secondary='user_favorite_profiles',
+        back_populates='users'
+    )
+    black_list = relationship(
+        'BlackList',
+        secondary='user_black_list',
         back_populates='users'
     )
 
@@ -34,7 +47,7 @@ class FavoriteProfile(Base):
 
     users = relationship(
         'User',
-        secondary=UserFavouriteProfile,
+        secondary='user_favorite_profiles',
         back_populates='favorite_profiles'
     )
 
@@ -42,15 +55,15 @@ class FavoriteProfile(Base):
         return f'Profile {self.id}: vk_id: {self.profile_id}'
 
 
-class BlasckList(Base):
+class BlackList(Base):
     __tablename__ = 'black_list'
     id = Column(Integer, primary_key=True)
     profile_id = Column(Integer, nullable=False)
 
     users = relationship(
         'User',
-        secondary=UserFavouriteProfile,
-        back_populates='favorite_profiles'
+        secondary='user_black_list',
+        back_populates='black_list'
     )
 
     def __str__(self):
